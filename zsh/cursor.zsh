ed3b0ebed937b9d5977741cb812039319973b737
JustPCode <107036402+JustCoderdev@users.noreply.github.com>
2024-01-16 18:22:18 +0100

    Added zsh configuration files

---
+++ zsh/cursor.zsh
@@ -0,0 +1,36 @@
+# justcoderdev zsh cursor
+
+# PROMPT=$'%F{white}%~ %B%F{blue}>%f%b '
+PROMPT=$' %F{grey}%~ %B%F{white}$%f%b '
+
+# Enable block cursor in normal mode
+cursor_mode() {
+	# This snippet comes from https://thevaluable.dev/zsh-install-configure-mouseless/
+    # that comes from this other page https://ttssh2.osdn.jp/manual/4/en/usage/tips/vim.html for cursor shapes
+
+    cursor_block='\e[2 q'
+    cursor_beam='\e[6 q'
+
+    function zle-keymap-select {
+        if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]];
+		then
+            echo -ne $cursor_block
+        elif [[ ${KEYMAP} == main ]] ||
+            [[ ${KEYMAP} == viins ]] ||
+            [[ ${KEYMAP} = '' ]] ||
+            [[ $1 = 'beam' ]];
+		then
+            echo -ne $cursor_beam
+        fi
+    }
+
+    zle-line-init() {
+        echo -ne $cursor_beam
+    }
+
+    zle -N zle-keymap-select
+    zle -N zle-line-init
+}
+
+cursor_mode
+
