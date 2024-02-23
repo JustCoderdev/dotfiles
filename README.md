efb693752a53620c8d367e0a5ddee8792a18af23
JustCoderdev <JustCode.rdev@gmail.com>
2024-02-23 21:02:07 +0100

    Added i3status custom configs

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ share with me any feedback or trick you may know :p
 ## Index
 
 Configuration files for
-- Alacritty \[v0.12.2\] ([GitHub](https://github.com/alacritty/alacritty/tree/9d9982df0ed3ab40a9b432e8e8b75d9c7f7bd882))
+- Alacritty \[v0.12.2\] ([Github](https://github.com/alacritty/alacritty/tree/9d9982df0ed3ab40a9b432e8e8b75d9c7f7bd882))
 - Clang ([Docs](https://clangd.llvm.org/config.html))
 - Clang-format ([Docs](https://releases.llvm.org/12.0.0/tools/clang/docs/ClangFormatStyleOptions.html))
+- i3 ([Docs](https://i3wm.org/docs/userguide.html) [statDocs](https://i3wm.org/docs/i3status.html))
 - Nvim
 - Zsh
 
@@ -26,6 +27,9 @@ C compiler (duh)
 
 ### Clang-format
 C file formatter
+
+### i3
+A window manager
 
 ### Nvim
 Textual code editor
--- zsh/completition.zsh
+++ zsh/completition.zsh
@@ -8,18 +8,18 @@ zstyle ':completion:*' menu select
 
 
 ##  Keybindings  ##
-# bindkey -M menuselect 'h' vi-backward-char
-# bindkey -M menuselect 'k' vi-up-line-or-history
-# bindkey -M menuselect 'j' vi-down-line-or-history
-# bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect '^h' vi-backward-char
+bindkey -M menuselect '^k' vi-up-line-or-history
+bindkey -M menuselect '^j' vi-down-line-or-history
+bindkey -M menuselect '^l' vi-forward-char
 
-# bindkey -M menuselect '^d' vi-forward-blank-word  # move to next group
-# bindkey -M menuselect '^u' vi-backward-blank-word # move to prev group
+bindkey -M menuselect '^d' vi-forward-blank-word  # move to next group
+bindkey -M menuselect '^u' vi-backward-blank-word # move to prev group
 
 # bindkey -M menuselect '0' beginning-of-buffer-or-history # move to leftmost column
 # bindkey -M menuselect '$' end-of-buffer-or-history       # move to rightmost column
 
-# bindkey -M menuselect 'u' undo
+bindkey -M menuselect '^u' undo
 # bindkey -M menuselect '\t' accept-line
 # bindkey -M menuselect '^c' send-break
 
