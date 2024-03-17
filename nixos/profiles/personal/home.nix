b5bc751eef67baa1ce3a38f57e9edd70111328b4
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-03-17 18:12:00 +0100

    NixOS build#16

--- nixos/profiles/personal/home.nix
+++ nixos/profiles/personal/home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, settings, ... }:
 
 {
-	home.username = settings.username;
+	home.username = ${settings.username};
 	home.homeDirectory = "/home/${settings.username}";
 
 	programs.home-manager.enable = true;
