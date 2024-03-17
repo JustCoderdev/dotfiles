233668aa826919cdcd85d6768f2e82a039467ea3
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-03-17 17:56:51 +0100

    NixOS build#16

--- nixos/modules/user/apps/dev/zsh.nix
+++ nixos/modules/user/apps/dev/zsh.nix
@@ -1,20 +1,22 @@
 { config, pkgs, settings, ... }:
-
-{
+let 
+	username = settings.username;
+	dotfiles = settings.dotfiles;
+in {
 	programs.zsh.enable = true;
 
 	home.packages = with pkgs; [
 		zsh
 	];
 
-	home.file."~" = {
-		source = "${settings.dotfilespath}/zsh/.zshrc";
+	home.file."/home/${username}" = {
+		source = "${dotfiles}/zsh/.zshrc";
 	};
 
-	home.file."~/.zsh" = {
-		source = "${settings.dotfilespath}/zsh";
+	home.file."/home/${username}/.zsh" = {
+		source = "${dotfiles}/zsh";
 		recursive = true;
 	};
 
-	users.users.${settings.username}.shell = pkgs.zsh;
+	users.users.${username}.shell = pkgs.zsh;
 }
