24d9004db0538d8b3b1392ac206180f6dca16d40
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-03-27 19:12:36 +0100

    Added nixos host profile

---
+++ nixos/profiles/installation/home.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, inputs, settings, ... }:
+
+{
+	home.username = settings.username;
+	home.homeDirectory = "/home/${settings.username}";
+
+	programs.home-manager.enable = true;
+
+	imports = [
+		../../modules/user/apps/desktop/i3.nix
+
+		../../modules/user/apps/dev/editor/neovim.nix
+
+		../../modules/user/apps/dev/alacritty.nix
+		../../modules/user/apps/dev/clang.nix
+		../../modules/user/apps/dev/zsh.nix
+	];
+
+	home.stateVersion = "23.05";
+}
