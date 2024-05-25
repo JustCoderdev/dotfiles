b06e2ab09c70526983e5055cde0c8685c07a5046
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:11 +0200

    Fixed nixos rebuild script not detecting hostname mismatches

--- nixos/rebuild.sh
+++ nixos/rebuild.sh
@@ -1,16 +1,31 @@
 # Check whether you are on a different system
 
-if [ -z "${1:-}" ]; then
-	echo -e "Hostname not passed, defaulting to \033[32m#${HOST}\033[0m"
-	sudo nixos-rebuild switch --show-trace --flake ".#${HOST}"
-else
-	echo -e "Requested rebuild for \033[32m\"$1\"\033[0m"
-
-	if [ "$1" != "$HOST" ]; then
-		echo -e "Updating flake file... (${HOST:---}) -> ($1)"
-		sudo sed -i "s/\(hostname = \).*/\1\"$1\";/" "./flake.nix"
-	fi
-
-	sudo nixos-rebuild switch --show-trace --flake ".#$1"
+if [ -f "./flake.nix" ]; then
+	HOST_FLAKE=$(awk '/hostname = / {print $3}' flake.nix)
+	HOST_FLAKE=$(echo $HOST_FLAKE | sed 's/"\(.*\)";/\1/')
 fi
 
+HOST_SHELL="${HOST}"
+HOST_INPUT="${1}"
+
+echo "HOST_FLAKE: ${HOST_FLAKE}"
+echo "HOST_SHELL: ${HOST_SHELL}"
+echo "HOST_INPUT: ${HOST_INPUT}"
+echo ""
+
+# Check for input
+if [ -z $HOST_INPUT ]; then
+	echo -e "Hostname not passed, defaulting to \033[32m#${HOST_SHELL}\033[0m"
+else
+	echo -e "Requested rebuild for \033[32m\"${HOST_INPUT}\"\033[0m"
+	HOST_SHELL=$HOST_INPUT
+fi
+
+# Update flake file
+if [ "${HOST_SHELL}" != "${HOST_FLAKE}" ]; then
+	echo -e "Updating flake... (${HOST_FLAKE:---}) -> ($HOST_SHELL)"
+	sudo sed -i "s/\(hostname = \).*/\1\"$1\";/" ./flake.nix
+fi
+
+# Rebuild system
+sudo nixos-rebuild switch --show-trace --flake ".#${HOST_SHELL}"
