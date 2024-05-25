98e97ea37d3f5e91f2388aa1d36e8b648570726e
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:05 +0200

    Added LSP, CMP and theme plugins

--- nvim/lua/perin/themes.lua
+++ nvim/lua/perin/themes.lua
@@ -2,17 +2,27 @@ declare_file("themes.lua")
 
 
 -- TRUECOLOR CAPABILITY --
-if os.getenv("COLORTERM") == "truecolor" then
-	log("Detected TrueColor capable terminal")
-	vim.opt.termguicolors = true
-else
-	vim.opt.termguicolors = false
-end 
+
+local color_capable = os.getenv("COLORTERM") == "truecolor"
+if color_capable then log("?", "Detected TrueColor capable terminal") end 
+
+vim.opt.termguicolors = color_capable
+
+
+-- 
+
+local function load_scheme(name)
+	local scheme_ok, _ = pcall(vim.cmd, string.format("colorscheme %s", name))
+
+	if(not scheme_ok) then
+		log_error(string.format("Error loading %s colorscheme", SETTINGS.default_colorscheme))
+	end
+end
 
 
 -- COLORSCHEME --
-local scheme_ok, _ = pcall(vim.cmd, string.format("colorscheme %s", SETTINGS.default_colorscheme))
-if(not file_ok) then
-	log_error(string.format("Error loading %s colorscheme", SETTINGS.default_colorscheme))
-	vim.cmd(string.format(":colorscheme %s", SETTINGS.fallback_colorscheme))
-end
+
+load_scheme("default")
+load_scheme(SETTINGS.fallback_colorscheme)
+load_scheme(SETTINGS.default_colorscheme)
+
