38c7aa4cc2851dc5efd231a7fe64824db40e5444
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:01 +0200

    NixOS build#29

---
+++ nixos/modules/user/apps/dev/alacritty.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, settings, ... }:
+let 
+	username = settings.username;
+	dotfiles = settings.dotfiles_path;
+in {
+	programs.alacritty.enable = true;
+	home.packages = with pkgs; [ alacritty ];
+
+	# Import configuration from dotfiles
+	home.file."/home/${username}/.config/alacritty/alacritty.yml" = {
+		source = "${dotfiles}/alacritty/alacritty.yml";
+	};
+}
