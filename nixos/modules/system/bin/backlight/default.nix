4c1a38dbec5b1e146fbc6f418d03e8519dda58aa
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:07 +0200

    NixOS build acer#81

--- nixos/modules/system/bin/backlight/default.nix
+++ nixos/modules/system/bin/backlight/default.nix
@@ -5,39 +5,38 @@ with lib;
 let
 	FLAGS="-xc -Wall -Wextra -Werror -Wpedantic -pedantic -pedantic-errors -std=c89 -fcolor-diagnostics -lm";
 	cfg = config.system.bin.backlight;
+	backlight-der = with pkgs; (callPackage stdenv.mkDerivation {
+		name = "backlight";
+		version = "1.0";
+
+		src = ./.;
+
+		# Compilation dependencies
+		nativeBuildInputs = [ clang ];
+		buildInputs = [ ];
+
+		buildPhase = ''
+			clang ${FLAGS} "backlight.c" -o "backlight"
+			chmod +x "backlight"
+		'';
+
+		installPhase = ''
+			mkdir -p $out/bin
+			cp backlight $out/bin
+		'';
+	});
 in
 
 {
 	config = mkIf cfg.enable {
 		security.sudo.extraRules = [{
 			commands = [{
-				command = "${pkgs.systemd}/bin/backlight";
+				command = "${backlight-der}";
 				options = [ "NOPASSWD" ];
 			}];
 			groups = [ "wheel" ];
 		}];
 
-		environment.systemPackages = with pkgs; [
-			(callPackage stdenv.mkDerivation {
-				name = "backlight";
-				version = "1.0";
-
-				src = ./.;
-
-				# Compilation dependencies
-				nativeBuildInputs = [ pkgs.clang ];
-				buildInputs = [ ];
-
-				buildPhase = ''
-					clang ${FLAGS} "backlight.c" -o "backlight"
-					chmod +x "backlight"
-				'';
-
-				installPhase = ''
-					mkdir -p $out/bin
-					cp backlight $out/bin
-				'';
-			})
-		];
+		environment.systemPackages = [ backlight-der ];
 	};
 }
