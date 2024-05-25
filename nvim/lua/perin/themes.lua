94fe039db7250b8d2da3d0e3b38f00bec35e6571
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:25:27 +0200

    Added Colorizer plugin to support colors

--- nvim/lua/perin/themes.lua
+++ nvim/lua/perin/themes.lua
@@ -15,21 +15,25 @@ end
 local function load_scheme(scheme)
 	if(scheme.require_truecolor and (not COLOR_CAPABLE)) then
 		log_error(string.format("Error loading %s colorscheme: colors not supported", scheme.name))
-		return
+		return true
 	end
 
 	local scheme_ok, _ = pcall(vim.cmd, string.format("colorscheme %s", scheme.name))
 	if(not scheme_ok) then
 		log_error(string.format("Error loading %s colorscheme: not found", scheme.name))
+		return true
 	end
+
+	return false
 end
 
 
 -- COLORSCHEME --
-
-load_scheme({ name = "default", require_truecolor = false })
-load_scheme(SETTINGS.fallback_colorscheme)
-load_scheme(SETTINGS.default_colorscheme)
+if load_scheme(SETTINGS.default_colorscheme) then
+	if load_scheme(SETTINGS.fallback_colorscheme) then
+		load_scheme({ name = "default", require_truecolor = false })
+	end
+end
 
 
 -- vim-whitespace options
