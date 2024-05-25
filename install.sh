07bd1e5a45aab9de260c5b584c607c61ff577fb8
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:10 +0200

    Stupid stuff #19

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ echo -e "Generating hardware configuration"
 
 sudo rm -df "${DOTFILES_PATH}/nixos/hosts/nixos"
 sudo mkdir -p "${DOTFILES_PATH}/nixos/hosts/nixos"
-sudo nixos-generate-config --show-hardware-config > "${DOTFILES_PATH}/nixos/hosts/nixos/hardware-configuration.nix"
+sudo nixos-generate-config --show-hardware-config | sudo tee "${DOTFILES_PATH}/nixos/hosts/nixos/hardware-configuration.nix"
 
 
 # Move boot commands
@@ -26,16 +26,16 @@ BOOT_FILE_PATH="${DOTFILES_PATH}/nixos/hosts/nixos/boot.nix"
 CONFIG_FILE_PATH="/etc/nixos/configuration.nix"
 
 sudo touch $BOOT_FILE_PATH
-echo -ne "{ ... }:\n\n{\n\t#Bootloader\n"  > $BOOT_FILE_PATH
-grep "boot" $CONFIG_FILE_PATH             >> $BOOT_FILE_PATH
+echo -ne "{ ... }:\n\n{\n\t#Bootloader\n" | sudo tee    $BOOT_FILE_PATH
+grep "boot" $CONFIG_FILE_PATH             | sudo tee -a $BOOT_FILE_PATH
 
-echo -ne "\n\tboot.loader.systemd-boot.configurationLimit = 5;\n"  >> $BOOT_FILE_PATH
-echo -ne "\tboot.loader.grub.configurationLimit = 5;\n"            >> $BOOT_FILE_PATH
+echo -ne "\n\tboot.loader.systemd-boot.configurationLimit = 5;\n" | sudo tee -a $BOOT_FILE_PATH
+echo -ne "\tboot.loader.grub.configurationLimit = 5;\n"           | sudo tee -a $BOOT_FILE_PATH
 
-echo -ne "\n\t#Virtualisation\n"          >> $BOOT_FILE_PATH
-grep "virtualisation" $CONFIG_FILE_PATH   >> $BOOT_FILE_PATH
-echo -ne "}\n"                            >> $BOOT_FILE_PATH
-git add .
+echo -ne "\n\t#Virtualisation\n"          | sudo tee -a $BOOT_FILE_PATH
+grep "virtualisation" $CONFIG_FILE_PATH   | sudo tee -a $BOOT_FILE_PATH
+echo -ne "}\n"                            | sudo tee -a $BOOT_FILE_PATH
+sudo git add .
 
 
 # Rebuild system
