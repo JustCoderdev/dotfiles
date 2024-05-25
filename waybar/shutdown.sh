5dfb9edd2be23ac26bf0e51d997735a03a656b19
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:18:02 +0200

    Changed waybar and finalized style

--- waybar/shutdown.sh
+++ waybar/shutdown.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
-shutdown -k 1
+# shutdown -k 1
 
-notification_message='The shutdown sequence has started'
-notification_body='The computer will shutdown in a minute. REMEMBER TO PUSH ALL UNCOMMITTED CHANGES!'
-notification_action=$(notify-send --action "R=Reboot" --urgency=critical "${notification_message}" "${notification_body}")
+# notification_message='The shutdown sequence has started'
+# notification_body='The computer will shutdown in a minute. REMEMBER TO PUSH ALL UNCOMMITTED CHANGES!'
+# notification_action=$(notify-send --action "R=Reboot" --urgency=critical "${notification_message}" "${notification_body}")
+# notification_action=$(dunstify --action "mount,Mount drive as /mnt/..." --action "eject=Eject drive" "AAA")
  # --action "C=Cancel" --action "N=Shut now"
-echo "Action: ${notification_action}"
+# echo "Action: ${notification_action}"
+
+
+
+
+ACTION=$(dunstify --action="default,Reply" --action="forwardAction,Forward" "Message Received")
+
+case "$ACTION" in
+"default")
+    echo reply_action
+    ;;
+"forwardAction")
+    echo forward_action
+    ;;
+"2")
+    echo handle_dismiss
+    ;;
+esac
+
+
+
 
 exit 1
+
 
 case $notification_action in
 	"R") shutdown -r now ;;
