78e539fe11e17941982cfcd624883c6c996aaf90
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:05 +0200

    NixOS build acer#32

--- nixos/modules/system/desktop/x11.nix
+++ nixos/modules/system/desktop/x11.nix
@@ -20,14 +20,16 @@
 			};
 
 			touchpad = {
-				clickMethod = "clickfinger";
-				scrollMethod = "twofinger";
+				accelProfile = "flat";       # flat, adaptive
+				clickMethod = "buttonareas"; # buttonareas, clickfinger
 
 				# dmesg | grep i8042
-				dev = "/devices/platform/i8042/serio1/input/input5";
+				# dev = "/devices/platform/i8042/serio1/input/input5";
+
+				middleEmulation = false;
+				scrollMethod = "twofinger";
 
 				tapping = true;
-				middleEmulation = false;
 				tappingButtonMap = "lrm";
 			};
 		};
