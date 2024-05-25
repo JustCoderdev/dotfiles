ed4f17e88111492af915a760183d681326b04996
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:07 +0200

    NixOS build#35

--- nixos/modules/user/apps/dev/neovim.nix
+++ nixos/modules/user/apps/dev/neovim.nix
@@ -1,17 +1,40 @@
 { config, pkgs, settings, ... }:
 let 
+	old_username = "perin";
 	username = settings.username;
 	dotfiles = settings.dotfiles_path;
 in {
 	programs.neovim.enable = true;
 	programs.neovim.defaultEditor = true;
-	programs.neovim.extraLuaConfig = "SETTINGS.user_name = ${username}";
+	programs.neovim.extraLuaConfig = ''
+		SETTINGS = {                              \n
+			user_name = ${username},          \n
+			default_colorscheme = {           \n
+				name = "onedark",         \n
+				require_truecolor = true  \n
+			},                                \n
+			fallback_colorscheme = {          \n
+				name = "habamax",         \n
+				require_truecolor = false \n
+			}                                 \n
+		}                                         \n
+                                                          \n
+		file = "init.lua";                                                                \n
+		local require_string = string.format("%s.%s", ${old_username}, file)              \n
+		local file_ok, _ = pcall(require, require_string)                                 \n
+		if (not file_ok) then                                                             \n
+			print(string.format(" /!\\  Error loading %s.lua file", require_string)); \n
+		end                                                                               \n
+	'';
 
 #	home.packages = with pkgs; [ neovim ];
 
 	# Import configuration from dotfiles
-	home.file."/home/${username}/.config/neovim" = {
-		source = "${dotfiles}/nvim";
+	home.file."/home/${username}/.config/neovim/lua/${username}" = {
+		source = "${dotfiles}/nvim/lua/${old_username}/";
 		recursive = true;
+	};
+	home.file."/home/${username}/.config/neovim/lua/${username}/init.lua" = {
+		source = "${dotfiles}/nvim/init.lua";
 	};
 }
