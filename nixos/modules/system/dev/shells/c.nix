257e1816b7a0e704bf5de3b3cca95bafc27b42ad
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:06 +0200

    Merging changes on acer with virtualmachine

--- nixos/modules/system/dev/shells/c.nix
+++ nixos/modules/system/dev/shells/c.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }:
 
-let
-	cshell = pkgs.mkShell {
-		buildInputs = with pkgs; [ clang ];
-	};
-in {
-	environment.systemPackages = with pkgs; [
-		(writeShellApplication {
-			name = "CShell";
-			runtimeInputs = with pkgs; [ zsh ];
-			text = "zsh --init-file ${cshell}";
-		})
+pkgs.mkShell {
+	inputsFrom = with pkgs; [default];
+	packages = with pkgs; [
+		glibcInfo
+		glibc
+
+		clang-tools
+		clang
+
+		gnumake
+		gdb
+
+		file
 	];
 }
