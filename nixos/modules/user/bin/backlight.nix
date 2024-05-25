a1db5f4bff14b4f25ce3bedc68f3ef16b784f08e
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:11 +0200

    NixOS build acer#27

--- nixos/modules/user/bin/backlight.nix
+++ nixos/modules/user/bin/backlight.nix
@@ -1,13 +1,35 @@
-{ pkgs, settings, ... }:
+{ stdenv, pkgs, settings }:
 let
 	dotfiles = settings.dotfiles_path;
-	i3Bin_path = "${dotfiles}/i3/scripts/bin";
+	FLAGS="-xc -Wall -Wextra -Werror -Wpedantic -pedantic -pedantic-errors -std=c89 -fcolor-diagnostics -lm";
 in
-{
-	environment.systemPackages = [(
-		pkgs.writeShellApplication {
-			name = "backlight";
-			text = (builtins.readFile "${i3Bin_path}/backlight");
-		}
-	)];
+stdenv.mkDerivation rec {
+	name = "backlight";
+	version = "1.0";
+
+	src = "${dotfiles}/i3/scripts";
+
+	# Compilation dependency
+	nativeBuildInputs = [ pkgs.clang ];
+	buildInputs = [ ];
+
+	buildPhase = ''
+		clang ${FLAGS} "backlight.c" -o "backlight"
+		chmod +x "backlight"
+	'';
+
+	installPhase = ''
+		mkdir -p $out/bin
+		cp backlight $out/bin
+	'';
 }
+# {
+# 	environment.systemPackages = [(
+# 		pkgs.writeShellApplication {
+# 			name = "backlight";
+# 			text = (builtins.readFile "${i3Bin_path}/backlight");
+# 		}
+# 	)];
+# }
+
+
