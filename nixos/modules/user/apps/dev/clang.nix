53647499fd24d021c8ddde74005831cd25454b76
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:08 +0200

    NixOS build#

---
+++ nixos/modules/user/apps/dev/clang.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, settings, ... }:
+let
+	username = settings.username;
+	dotfiles = settings.dotfiles_path;
+	cachepath = settings.cache_path;
+in {
+	# Configuration for:
+	#  - clangd
+	#  - clang-format
+
+	home.packages = with pkgs; [
+		clang-tools
+	];
+
+	# Import configuration from dotfiles
+	home.file."/home/${username}/.config/clangd" = {
+		source = "${dotfiles}/clangd/";
+		recursive = true;
+	};
+	home.file."/home/${username}/.clang-format" = {
+		source = "${dotfiles}/clangd/.clang-format";
+	};
+}
