16cb9b8c8878f73b35b464fcecee2c55d7106b3e
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:07 +0200

    NixOS build#15

--- nixos/profiles/personal/home.nix
+++ nixos/profiles/personal/home.nix
@@ -1,13 +1,20 @@
-{ config, pkgs, settings, ... }:
+{ config, pkgs, inputs, settings, ... }:
 
 {
 	home.username = settings.username;
 	home.homeDirectory = "/home/${settings.username}";
-	home-manager.useUserPackages = true;
 
 	imports = [
+		inputs.home-manager.nixosModules.home-manager
 		../../modules/user/apps/dev/zsh.nix
-	]
+	];
+
+	home-manager.useGlobalPackages = true;
+	home-manager.useUserPackages = true;
+	# home-manager.users.${settings.username}.imports = [
+	# 	../../modules/user/apps/dev/zsh.nix
+	# ]
+
 
 	# Let Home Manager install and manage itself.
 	programs.home-manager.enable = true;
