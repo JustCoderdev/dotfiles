88e48b9d6d61f384c9e1bcf1a48ae4b75ea63735
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:08 +0200

    NixOS build acer#92

--- nixos/modules/system/dev/c.nix
+++ nixos/modules/system/dev/c.nix
@@ -5,18 +5,36 @@ let cfg = config.system.dev.c; in
 
 {
 	config = mkIf cfg.enable {
+		documentation = {
+			enable = true;
+
+			nixos.enable = true;
+			dev.enable = true;
+
+			man = {
+				enable = true;
+				generateCaches = false;
+			};
+		};
+
 		environment.systemPackages = with pkgs; [
 			glibcInfo
 			glibc
+
+			man-pages
+			man-pages-posix
+			clang-manpages
+			linux-manual
+			stdmanpages
+			stdman
 
 			clang-tools
 			clang
 			gcc
 
 			gnumake
-			gdb
-
 			file
+			gdb
 		];
 	};
 
