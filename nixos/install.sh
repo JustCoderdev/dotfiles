bc7c3926298e1d12e5eeffd8d786495812f90f30
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:02 +0200

    Added nixos install script

---
+++ nixos/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DOTFILES_PATH="/.dotfiles"
+
+# Clone dotfiles
+nix-shell -p git --command "git clone https://github.com/JustCoderdev/dotfiles.git ${DOTFILES_PATH}"
+
+# Generate harware configuration
+sudo nixos-generate-config --show-hardware-config > "${DOTFILES_PATH}/nixos/hosts/nixos/hardware-config.nix"
+
+# Move boot commands
+BOOT_FILE_PATH="${DOTFILES_PATH}/nixos/hosts/nixos/boot.nix"
+CONFIG_FILE_PATH="/etc/nixos/configuration.nix"
+
+touch $BOOT_FILE_PATH
+echo -ne "{ ... }:\n\n{\n\t#Bootloader\n"  > $BOOT_FILE_PATH
+grep "boot" $CONFIG_FILE_PATH             >> $BOOT_FILE_PATH
+
+echo -ne "\n\t#Virtualisation\n"          >> $BOOT_FILE_PATH
+grep "virtualisation" $CONFIG_FILE_PATH   >> $BOOT_FILE_PATH
+
+echo -ne "}\n"                            >> $BOOT_FILE_PATH
+
+# Rebuild system
+sudo nixos-rebuild switch --show-trace --flake "${DOTFILES_PATH}/nixos#nixos"
