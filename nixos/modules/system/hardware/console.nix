44db1a46e4fc407d2072ec9a15c64b00ba38987a
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:03 +0200

    NixOS build#79

--- nixos/modules/system/hardware/console.nix
+++ nixos/modules/system/hardware/console.nix
@@ -1,14 +1,17 @@
 { ... }:
 
 {
-	imports = [
-		../desktop/fonts.nix
-	];
+	# imports = [
+	# 	../desktop/fonts.nix
+	# ];
 
 	# Configure console keymap
 	console = {
 		enable = true;
-		font = "Roboto Mono";
+
+		# Font too big
+		# font = "Roboto Mono";
+		# font = "${pkgs.roboto-mono}/share/fonts/truetype/RobotoMono-Medium.ttf";
 
 		# keyMap = "it2";
 		useXkbConfig = true;
