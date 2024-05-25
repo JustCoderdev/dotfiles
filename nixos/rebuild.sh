85c75c48aaa4b35dfb8e3b6d9f39fcfb49a103fe
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:03 +0200

    Fixed nixos rebuild scripts

--- nixos/rebuild.sh
+++ nixos/rebuild.sh
@@ -1,9 +1,16 @@
 # Check whether you are on a different system
-if [ -z "${1:-$HOST}" ]; then
-	echo -e "Host not passed, defaulting to \033[32m#\033[0m"
+
+if [ -z $1 ]; then
+	echo -e "Hostname not passed, defaulting to \033[32m#${HOST}\033[0m"
+	sudo nixos-rebuild switch --show-trace --flake ".#${HOST}"
 else
-	echo -e "Requested rebuild for \"\033[32m${1:-$HOST}\033[0m\". Updating flake file..."
-	sed -i "s/\(hostname = \).*/\1\"${1:-$HOST}\";/" "./flake.nix"
+	echo -e "Requested rebuild for \"\033[32m$1\033[0m\""
+
+	if [ $1 != $HOST ]; then
+		echo -e "Updating flake file... (${HOST}) -> ($1)"
+		sed -i "s/\(hostname = \).*/\1\"$1\";/" "./flake.nix"
+	fi
+
+	sudo nixos-rebuild switch --show-trace --flake ".#$1"
 fi
 
-sudo nixos-rebuild switch --show-trace --flake ".#${1:-$HOST}"
