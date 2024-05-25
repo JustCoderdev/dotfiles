e7e7ce20e85285afb98a70976b11df2b11dc9db4
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:17:08 +0200

    NixOS build#39

--- nixos/modules/user/apps/dev/neovim.nix
+++ nixos/modules/user/apps/dev/neovim.nix
@@ -1,8 +1,8 @@
 { config, pkgs, settings, ... }:
 let 
-	old_username = "perin";
 	username = settings.username;
 	dotfiles = settings.dotfiles_path;
+	cachepath = settings.cache_path;
 in {
 	programs.neovim.enable = true;
 	programs.neovim.defaultEditor = true;
@@ -11,29 +11,21 @@ in {
 
 	# Import configuration from dotfiles
 	home.file."/home/${username}/.config/neovim/init.lua".text = ''
-		SETTINGS = {                              \n
-			user_name = ${username},          \n
-			default_colorscheme = {           \n
-				name = "onedark",         \n
-				require_truecolor = true  \n
-			},                                \n
-			fallback_colorscheme = {          \n
-				name = "habamax",         \n
-				require_truecolor = false \n
-			}                                 \n
-		}                                         \n
-                                                          \n
+		SETTINGS.user_name = "${username}"                                                \n
+		SETTINGS.cache_path = "${cachepath}/nvim"                                         \n
+
+		print("hi") \n
+		 
 		file = "init.lua";                                                                \n
-		local require_string = string.format("%s.%s", ${old_username}, file)              \n
+		local require_string = string.format("%s.%s", ${username}, file)                  \n
 		local file_ok, _ = pcall(require, require_string)                                 \n
 		if (not file_ok) then                                                             \n
 			print(string.format(" /!\\  Error loading %s.lua file", require_string)); \n
 		end                                                                               \n
-		\n
 	'';
 
 	home.file."/home/${username}/.config/neovim/lua/${username}" = {
-		source = "${dotfiles}/nvim/lua/${old_username}/";
+		source = "${dotfiles}/nvim/lua/${username}/";
 		recursive = true;
 	};
 	home.file."/home/${username}/.config/neovim/lua/${username}/init.lua" = {
