958cb6c3f45b7f6d000720288ef6b43f5edcd6f5
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 18:22:42 +0200

    NixOS build virtualmachine#170

--- nixos/modules/unofficial/dotfiles-backup.nix
+++ nixos/modules/unofficial/dotfiles-backup.nix
@@ -7,7 +7,9 @@ stdenv.mkDerivation {
 	src = settings.dotfiles_path;
 
 	installPhase = ''
-		mkdir -p $out/run/current-dotfiles
-		cp -r . $out/run/current-dotfiles
+		mkdir -p $out/current-dotfiles
+		cp -r . $out/current-dotfiles
 	'';
 }
+
+# /nix/store/i3mxngx5sk1lxg1ccbv3sycgxw4avan9-dotfiles-backup
