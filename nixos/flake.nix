13012eadd91623a054e6072f49bbe800e4bdc03f
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:06 +0200

    NixOS build acer#74

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -19,7 +19,7 @@
 	};
 	outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, hyprland, nixd }@inputs:
 		let
-			_hostname = "virtualmachine";
+			_hostname = "acer";
 			settings = import ./hosts/${_hostname}/settings.nix;
 
 			pkgs = nixpkgs.legacyPackages.${settings.system};
