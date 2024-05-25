234cd8be1f94633dd4c0b1bb781b96e9b2c937b2
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-25 11:24:03 +0200

    Changed nixos install script to have labels

--- nixos/rebuild.sh
+++ nixos/rebuild.sh
@@ -4,9 +4,9 @@ if [ -z $1 ]; then
 	echo -e "Hostname not passed, defaulting to \033[32m#${HOST}\033[0m"
 	sudo nixos-rebuild switch --show-trace --flake ".#${HOST}"
 else
-	echo -e "Requested rebuild for \"\033[32m$1\033[0m\""
+	echo -e "Requested rebuild for \033[32m\"$1\"\033[0m"
 
-	if [ $1 != $HOST ]; then
+	if [ "$1" != "$HOST" ]; then
 		echo -e "Updating flake file... (${HOST}) -> ($1)"
 		sed -i "s/\(hostname = \).*/\1\"$1\";/" "./flake.nix"
 	fi
