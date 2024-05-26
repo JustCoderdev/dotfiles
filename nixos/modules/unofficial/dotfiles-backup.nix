ee3c9e481e6c9293cf69d3c2d189fdb208e8213e
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-05-26 15:10:26 +0200

    NixOS build virtualmachine#178

--- nixos/modules/unofficial/dotfiles-backup.nix
+++ nixos/modules/unofficial/dotfiles-backup.nix
@@ -1,15 +1,29 @@
-{ stdenv, settings }:
+{ pkgs, settings }:
 
-stdenv.mkDerivation {
-	name = "dotfiles-backup";
+let
+	dotfiles-backup = pkgs.stdenv.mkDerivation {
+		name = "dotfiles-backup";
 
-	version = "1.0";
-	src = settings.dotfiles_path;
+		version = "1.0";
+		src = settings.dotfiles_path;
 
-	installPhase = ''
-		mkdir -p $out/current-dotfiles
-		cp -r . $out/current-dotfiles
-	'';
+		installPhase = ''
+			mkdir -p $out/current-dotfiles
+			cp -r . $out/current-dotfiles
+		'';
+	};
+in
+
+{
+	environment.systemPackages =  [
+		(pkgs.callPackage dotfiles-backup)
+		(pkgs.writeShellApplication {
+			name = "current-dotfiles";
+			text = ''
+				# /nix/store/i3mxngx5sk1lxg1ccbv3sycgxw4avan9-dotfiles-backup
+				echo ${dotfiles-backup}
+				cd ${dotfiles-backup}
+			'';
+		})
+	];
 }
-
-# /nix/store/i3mxngx5sk1lxg1ccbv3sycgxw4avan9-dotfiles-backup
