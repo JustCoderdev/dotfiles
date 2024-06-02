08dde92eea17dd2832b78cbddec2c55ca0c186a0
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-06-02 09:59:48 +0200

    NixOS build virtualmachine#186

--- nixos/modules/user/dev/zsh.nix
+++ nixos/modules/user/dev/zsh.nix
@@ -1,19 +1,22 @@
-{ config, pkgs, settings, ... }:
+{ settings, ... }:
 let
 	username = settings.username;
 	hostname = settings.hostname;
 	dotfiles = settings.dotfiles_path;
 in {
-	programs.zsh.enable = true;
-#	home.packages = with pkgs; [ zsh ];
+	programs.zsh = {
+		enable = true;
+		prezto.enable = false;
 
 	# Import configuration from dotfiles
-	programs.zsh.initExtra = ''
+		initExtra = ''
 export DOT_NIXOS=1;
 export HOST=${hostname};
 export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH;
 source "${dotfiles}/zsh/.zshrc";
-'';
+		'';
+	};
+
 	home.file."/home/${username}/.zsh" = {
 		source = "${dotfiles}/zsh";
 		recursive = true;
