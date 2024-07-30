9478a73ff22647b33f0e451a98878d85532e5c6c
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-07-30 19:03:56 +0200

    NixOS build msi#29

--- nixos/hosts/msi/boot.nix
+++ nixos/hosts/msi/boot.nix
@@ -3,7 +3,7 @@
 {
 	#Bootloader
 	boot.loader.systemd-boot.enable = true;
-	boot.loader.systemd-boot.configurationLimit = 5;
+#	boot.loader.systemd-boot.configurationLimit = 5;
 	boot.loader.efi.canTouchEfiVariables = true;
 
 	#Virtualisation
