6fd991786d5e06217a0260f471b3c2deb2288088
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-07-30 17:12:25 +0200

    NixOS build msi#22

--- nixos/hosts/msi/boot.nix
+++ nixos/hosts/msi/boot.nix
@@ -9,9 +9,11 @@
 	#Virtualisation
 
 	#Mounts
-#	fileSystems."/mnt/WDC_WD10" = {
-#		device = "/dev/disk/by-uuid/2a9ccfda-3e0f-410c-9577-b7b2e15e3426";
-#		fsType = "auto";
-#		options = [ "defaults" "user" "rw" ];
-#	};
+	system.nixos.tags = [ "ntfsON" ];
+	boot.supportedFilesystems = [ "ntfs" ];
+	fileSystems."/mnt/WDC_WD10" = {
+		device = "/dev/disk/by-uuid/0EAE7AF7228F3AF1";
+		fsType = "ntfs-3g";
+		options = [ "defaults" "user" "rw" ];
+	};
 }
