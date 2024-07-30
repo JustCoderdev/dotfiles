1bed7b3e57865ddac4677bb21bd339ac08f7ea02
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-07-30 07:55:46 +0200

    NixOS build msi#9

--- nixos/modules/system/desktop/wayland/hyprland.nix
+++ nixos/modules/system/desktop/wayland/hyprland.nix
@@ -34,14 +34,15 @@ in
 #			packages = [ pkgs.dconf ];
 #		};
 #
-		services.gnome.gnome-keyring.enable = true;
-		security.pam.services = {
-			swaylock = { };
-#			swaylock.text = ''
-#				auth include login
-#			'';
-			login.enableGnomeKeyring = true;
-#			#gtklock = {};
-		};
+#		services.gnome.gnome-keyring.enable = true;
+#		security.pam.services = {
+##			swaylock = { };
+##			swaylock.text = ''
+##				auth include login
+##			'';
+#
+#			login.enableGnomeKeyring = true;
+##			#gtklock = {};
+#		};
 	};
 }
--- nixos/modules/system/desktop/wayland/sddm.nix
+++ nixos/modules/system/desktop/wayland/sddm.nix
@@ -9,31 +9,32 @@ let cfg = config.system.desktop.hyprland; in
 
 			wayland.enable = true;
 			enableHidpi = true;
-			setupScript = ''
-xrandr --output HDMI-A-1 --off
-xrandr --output Unknown-1 --off
-xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
-'';
+#			setupScript = ''
+#xrandr --output HDMI-A-1 --off
+#xrandr --output Unknown-1 --off
+#xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
+#'';
 
 			autoNumlock = true;
 			theme = "where_is_my_sddm_theme_qt5";
 		};
 
-		services.xserver.displayManager.setupCommands = ''
-xrandr --output HDMI-A-1 --off
-xrandr --output Unknown-1 --off
-xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
-'';
+#		services.xserver.displayManager.setupCommands = ''
+#xrandr --output HDMI-A-1 --off
+#xrandr --output Unknown-1 --off
+#xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
+#'';
 
 		environment.systemPackages = [
 			(pkgs.where-is-my-sddm-theme.override {
 				variants = [ "qt5" ];
 				themeConfig.General = {
+					passwordInputWidth = "0.3";
 					passwordInputBackground = "#252525";
 					passwordInputRadius = 4;
 					passwordInputCursorVisible=false;
 
-					passwordFontSize=24;
+					passwordFontSize=28;
 					passwordCursorColor = "#ffffff";
 					passwordTextColor = "#ffffff";
 
