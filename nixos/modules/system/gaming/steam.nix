563749143bb4c03d28ba1c6c0b2e84ef79cdcb41
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-07-30 17:38:19 +0200

    NixOS build msi#23

--- nixos/modules/system/gaming/steam.nix
+++ nixos/modules/system/gaming/steam.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }:
 
+let
+	hyprland = config.system.desktop.hyprland;
+in
+
 {
 	config = lib.mkIf config.system.gaming.enable {
 		programs.steam = {
@@ -8,6 +12,7 @@
 
 			#proton-ge-bin
 			#extraCompatPackages = [  ];
+			extest.enable = (lib.mkIf hyprland.enable true);
 		};
 	};
 }
