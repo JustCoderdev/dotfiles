ad11d4849a70858356ccbb884536f3ce4e1a6cab
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-08-03 12:03:31 +0200

    NixOS build msi#40

--- nixos/modules/system/desktop/xserver/i3.nix
+++ nixos/modules/system/desktop/xserver/i3.nix
@@ -6,15 +6,25 @@ let cfg = config.system.desktop.xfce; in
 	config = lib.mkIf cfg.enable {
 		services = {
 			displayManager.defaultSession = lib.mkForce "none+i3";
-			xserver.windowManager.i3 = {
-				enable = true;
-				extraPackages = with pkgs; [
-					dmenu
-					i3status
-					playerctl
-					# i3lock
-				];
+			xserver = {
+				windowManager.i3 = {
+					enable = true;
+					extraPackages = with pkgs; [
+						dmenu
+						i3status
+						playerctl
+						# i3lock
+					];
+				};
+
+				displayManager.setupCommands = ''
+xrandr --output HDMI-A-1 --off
+xrandr --output Unknown-1 --off
+xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
+'';
+
 			};
+
 		};
 
 		# Remember windows size stuff
