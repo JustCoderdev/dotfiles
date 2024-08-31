5b5ebcd3440194444d4905a393e22bc5ddb43858
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-08-31 14:27:39 +0200

    NixOS build msi#66

--- nixos/modules/system/gaming/gamemode.nix
+++ nixos/modules/system/gaming/gamemode.nix
@@ -1,7 +1,12 @@
-{ config, lib, ... }:
+{ config, lib, settings, ... }:
+
+let
+	username = settings.username;
+in
 
 {
 	config = lib.mkIf config.system.gaming.enable {
+		users.users.${username}.extraGroups = [ "gamemode" ];
 		programs.gamemode.enable = true;
 
 		# To use gamemode add to launch options
