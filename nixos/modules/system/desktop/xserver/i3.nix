505aa09359ad166b53ba7b8d777fcb29db163116
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-09-04 10:50:40 +0200

    NixOS build msi#74

--- nixos/modules/system/desktop/xserver/i3.nix
+++ nixos/modules/system/desktop/xserver/i3.nix
@@ -17,12 +17,15 @@ let cfg = config.system.desktop.xfce; in
 					];
 				};
 
+#xrandr --output HDMI-A-1 --off
+#xrandr --output Unknown-1 --off
+#xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
 				displayManager.setupCommands = ''
-xrandr --output HDMI-A-1 --off
-xrandr --output Unknown-1 --off
-xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
-'';
-
+					LEFT='Unknown-1'
+					CENTER='HDMI-A-1'
+					RIGHT='desc:ASUSTek COMPUTER INC ASUS VA24E L7LMTF289885'
+					${pkgs.xorg.xrandr}/bin/xrandr --output $CENTER --rotate left --output $LEFT --rotate left --left-of $CENTER --output $RIGHT --right-of $CENTER
+				'';
 			};
 
 		};
