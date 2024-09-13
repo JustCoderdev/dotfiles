58ad99dc8fd55507152d39ea29c78ac0f20e8cb5
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-09-13 16:05:37 +0200

    NixOS build msi#83

--- nixos/modules/common/core/nvidia.nix
+++ nixos/modules/common/core/nvidia.nix
@@ -9,13 +9,16 @@ in
 
 		system.nixos.tags = [ "nvidia" ];
 		services.xserver.videoDrivers = [ "nvidia" ];
-		environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+		#environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
 
 		hardware.nvidia = {
 			modesetting.enable = true;
 
 			# GPU Support for GeForce GTX 1050Ti
-			package = config.boot.kernelPackages.nvidiaPackages.stable;
+			#package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+			# (installs 550)
+			package = config.boot.kernelPackages.nvidiaPackages.production;
 
 			# Enable this if you have graphical issues
 			powerManagement.enable = false;
