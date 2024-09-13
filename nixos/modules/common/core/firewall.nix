a304b7a290f9bd7548f987758cf40ffe6a986672
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-09-13 20:24:41 +0200

    NixOS build msi#86

--- nixos/modules/common/core/firewall.nix
+++ nixos/modules/common/core/firewall.nix
@@ -7,7 +7,18 @@
 
 		# Open ports in the firewall.
 		# interfaces.<name>.
-		allowedTCPPorts = [ 25565 ];
-		allowedUDPPorts = [ 25565 ];
+		allowedTCPPorts = [
+			25565 # Minecraft
+			5222  # Satisfactory
+			6666  # Satisfactory
+		];
+		allowedUDPPorts = [
+			25565 # Minecraft
+			5222  # Satisfactory
+			6666  # Satisfactory
+		];
+		allowedUDPPortRanges = [
+			{ from = 7777; to = 7827; } # Satisfactory
+		];
 	};
 }
