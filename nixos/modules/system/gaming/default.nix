0ce70e19ee8b719b804a134262169a581d814162
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2024-11-20 18:13:54 +0100

    NixOS build msi#123

--- nixos/modules/system/gaming/default.nix
+++ nixos/modules/system/gaming/default.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
-let cfg = config.system.gaming; in
+let
+	cfg = config.system.gaming;
+in
 
 {
 	imports = [
@@ -20,33 +22,39 @@ let cfg = config.system.gaming; in
 
 	config = lib.mkIf cfg.enable {
 
-		environment.systemPackages = with pkgs; [
-			discord # Run with --disable-gpu
+		environment.systemPackages = (lib.mkMerge [
+			[
+				(pkgs.callPackage  ../../unofficial/apple-fonts.nix {})
+			]
 
-			goverlay # Edit overlay
-			mangohud # Overlay
+			(with pkgs; [
+				discord # Run with --disable-gpu
 
-			# To use mangohud add to launch options
-			# mangohud %command%
+				goverlay # Edit overlay
+				mangohud # Overlay
 
-			# To use gamemode add to launch options
-			# gamemoderun %command%
+				# To use mangohud add to launch options
+				# mangohud %command%
 
-			# For both
-			# gamemoderun mangohud %command%
+				# To use gamemode add to launch options
+				# gamemoderun %command%
+
+				# For both
+				# gamemoderun mangohud %command%
 
 #			heroic         # Epic Games - gog
 #			prismlauncher  # Minecraft launcher
-			modrinth-app   # Minecraft launcher
+				modrinth-app   # Minecraft launcher
 
-			(lutris.override {   # Game hub
-				extraLibraries = pkgs: [ ];
-				extraPkgs = pkgs: [ pkgs.winetricks ];
-			})
+				(lutris.override {   # Game hub
+					extraLibraries = pkgs: [ ];
+					extraPkgs = pkgs: [ pkgs.winetricks ];
+				})
 #			gamehub  # Game hub
 
 #			bottles     # Vine game manager
 #			cartridges  # Game hub
-		];
+			])
+		]);
 	};
 }
