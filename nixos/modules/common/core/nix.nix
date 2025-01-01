d3115d1de238e3ccbabd3d548d79f9cca11b2818
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-01-01 14:18:52 +0100

    Fixed merge conflict in nix.nix

--- nixos/modules/common/core/nix.nix
+++ nixos/modules/common/core/nix.nix
@@ -1,54 +1,58 @@
-{ lib, pkgs, settings, ... }:
+{ config, lib, pkgs, settings, ... }:
 
 let
+	cfg = config.nix;
 	nix-serve-port = 56552;
 in
 
 {
-	system.nixos.tags = [ "${settings.hostname}" ];
-
-	nix = {
-		optimise.automatic = true;
-		gc = {
-			automatic = true;
-			dates = "monthly";
-
-			# Keep the last 5 generations
-			options = "--delete-older-than 15d";
-		};
-		settings = {
-			auto-optimise-store = true;
-			allowed-users = [ "root" "@wheel" ];
-			substituters = [
-				"http://nixcache.local:${toString nix-serve-port}"
-				"https://cache.nixos.org/"
-			];
-
-			experimental-features = [ "nix-command" "flakes" ];
-			warn-dirty = false;
+	options.nix = {
+		serve-store.enable = lib.mkOption {
+			type = lib.types.bool;
+			description = "Enable nix-store service";
+			default = false;
 		};
 	};
 
-#	boot.loader.grub.extraEntries = ''
-## Power options
-#menuentry "Reboot" {
-#	reboot
-#}
-#
-#menuentry "Shutdown" {
-#	halt
-#}
-#'';
+	config = {
+		system.nixos.tags = [ "${settings.hostname}" ];
 
-	services.journald.extraConfig = "SystemMaxUse=1G";
-	programs.nix-ld = {
-		enable = true;
-		libraries = [ pkgs.glibc ];
+		nix = {
+			optimise.automatic = true;
+			gc = {
+				automatic = true;
+				dates = "monthly";
+
+				# Keep the last 5 generations
+				options = "--delete-older-than 15d";
+			};
+			settings = {
+				auto-optimise-store = true;
+				allowed-users = [ "root" "@wheel" ];
+
+				experimental-features = [ "nix-command" "flakes" ];
+				warn-dirty = false;
+			};
+		};
+
+		services.journald.extraConfig = "SystemMaxUse=1G";
+
+		services.nix-serve = {
+			enable = cfg.serve-store.enable;
+			openFirewall = true;
+			port = nix-serve-port;
+		};
+
+		programs.nix-ld = {
+			enable = true;
+			libraries = [ pkgs.glibc ];
+		};
+
+		nixpkgs.config = let pkgs = settings.special_pkgs; in {
+			permittedInsecurePackages = pkgs.insecure;
+			allowUnfreePredicate = pkg:
+				builtins.elem (lib.getName pkg) pkgs.unfree;
+		};
 	};
 
-	nixpkgs.config = let pkgs = settings.special_pkgs; in {
-		permittedInsecurePackages = pkgs.insecure;
-		allowUnfreePredicate = pkg:
-			builtins.elem (lib.getName pkg) pkgs.unfree;
-	};
 }
