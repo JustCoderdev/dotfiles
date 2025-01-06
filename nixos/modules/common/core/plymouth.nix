21f2db44b52b1f2fb7aaefdb7f8c34e079e86045
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-01-06 10:18:20 +0100

    NixOS build msi#141

--- nixos/modules/common/core/plymouth.nix
+++ nixos/modules/common/core/plymouth.nix
@@ -1,38 +1,40 @@
 { pkgs, lib, settings, ... }:
 
+let
+	# Took this bit from the catppuccini plymouth theme repo
+	# Link: <https://github.com/NixOS/nixpkgs/blob/nixos-23.11/pkgs/data/themes/catppuccin-plymouth/default.nix>
+	theme-name = "darnix";
+	darnix-theme = pkgs.stdenv.mkDerivation rec {
+		name = "darnix-plymouth-theme";
+		src = pkgs.fetchFromGitHub {
+			owner = "JustCoderdev";
+			repo = "dotfiles";
+			rev = "9af46b860ee3a076f822a0dd028c86a710682b14";
+			hash = "sha256-/pEB8Q8ueWq/jZUChAKjgt2dPBHYazYxwE3ghhV/oZw=";
+		};
+
+		sourceRoot = "${src.name}/plymouth/themes/${theme-name}";
+
+		installPhase = ''
+			runHook preInstall
+			sed -i 's:\(^ImageDir=\)/usr:\1'"$out"':' ${theme-name}.plymouth
+			sed -i 's:\(^ScriptFile=\)/usr:\1'"$out"':' ${theme-name}.plymouth
+			mkdir -p $out/share/plymouth/themes/${theme-name}
+			cp -r * $out/share/plymouth/themes/${theme-name}
+			runHook postInstall
+		'';
+	};
+in
+
 {
-	boot.kernelParams = [ "quiet" ];
+	boot.kernelParams = [ "quiet" "splash" "plymouth.debug" ];
 	boot.initrd.systemd.enable = true;
+
 	boot.plymouth = lib.mkIf (!settings.runningVM) {
 		enable = true;
 
 		# logo = ../.;
 		theme = "darnix";
-		themePackages = let
-			# Took this bit from the catppuccini plymouth theme repo
-			# Link: <https://github.com/NixOS/nixpkgs/blob/nixos-23.11/pkgs/data/themes/catppuccin-plymouth/default.nix>
-			theme-name = "darnix";
-			darnix = pkgs.stdenv.mkDerivation rec {
-				name = "darnix-plymouth-theme";
-				src = pkgs.fetchFromGitHub {
-					owner = "JustCoderdev";
-					repo = "dotfiles";
-					rev = "9af46b860ee3a076f822a0dd028c86a710682b14";
-					hash = "sha256-/pEB8Q8ueWq/jZUChAKjgt2dPBHYazYxwE3ghhV/oZw=";
-				};
-
-				sourceRoot = "${src.name}/plymouth/themes/${theme-name}";
-
-				installPhase = ''
-					runHook preInstall
-					sed -i 's:\(^ImageDir=\)/usr:\1'"$out"':' ${theme-name}.plymouth
-					sed -i 's:\(^ScriptFile=\)/usr:\1'"$out"':' ${theme-name}.plymouth
-					mkdir -p $out/share/plymouth/themes/${theme-name}
-					cp -r * $out/share/plymouth/themes/${theme-name}
-					runHook postInstall
-				'';
-			};
-		in
-		[ darnix ];
+		themePackages = [ darnix-theme ];
 	};
 }
