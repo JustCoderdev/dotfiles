53e373f381cf57d23615a9b83e8ce6bf775c79f0
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-01-25 14:47:04 +0100

    NixOS build msi#188

--- nixos/hosts/msi/configuration.nix
+++ nixos/hosts/msi/configuration.nix
@@ -35,74 +35,23 @@ pactl load-module module-loopback source=MCVirtualSink.monitor sink=alsa_output.
 
 	# Network settings
 	networking = {
-		# DNS Records
+
 		hosts = {
-			"10.10.30.60"   = [ "nixcache.local" ]; # Quiss
-		};
-	};
-
-	# Network bridging
-	# src: <https://www.reddit.com/r/NixOS/comments/1i89lh2/comment/m8s1g8t/?context=3>
-
-	# Enable kernel packet forwarding
-	boot.kernel.sysctl = {
-		"net.ipv4.conf.all.forwarding" = true;
-		"net.ipv6.conf.all.forwarding" = true;
-	};
-
-	services.dnsmasq = {
-		enable = true;
-		resolveLocalQueries = false;
-		settings = {
-			# dns
-			server = [
-				"193.110.81.0" # https://www.dns0.eu/it
-				"185.253.5.0"  # https://www.dns0.eu/it
-			];
-
-			domain-needed = true;
-			bogus-priv = true;
-			no-resolv = true;
-			cache-size = 1000;
-
-			# dhcp
-			dhcp-range = [ "br-lan,10.0.0.2,10.0.0.14,1h" ];
-			dhcp-leasefile = "/var/lib/dnsmasq/dnsmasq.leases";
-			dhcp-host = "10.0.0.1";
-
-			interface = "eno1";
-			no-hosts = true;
-		};
-	};
-
-	networking = {
-		nftables.enable = true;
-		firewall.trustedInterfaces = [ "eno1" ];
-		networkmanager.unmanaged = [ "interface-name:eno1" ];
-
-		nat = {
-			enable = true;
-			internalIPs = [ "10.0.0.0/28" ];
-			internalInterfaces = [ "eno1" ];
-
-			forwardPorts = [ { # 10.0.0.11:22 >>#<< 192.168.7.142:4022
-				proto = "tcp";
-				sourcePort = 4022;
-				destination = "10.0.0.11:22";
-			} ];
-
-			externalInterface = "wlp3s0";
+			"10.0.0.11"   = [ "nixcache.local" ]; # Quiss
 		};
 
-		interfaces.eno1 = {
-			useDHCP = false;
-			ipv4.addresses = [
-				{
-					address = "10.0.0.1";
-					prefixLength = 24;
-				}
-			];
-		};
+		# Network switching
+		bridges.br0.interfaces = [ "eno1" "wlp3s0" ];
+
+#		interfaces.br0 = {
+#			useDHCP = true;
+#			ipv4.addresses = [
+#				{
+#					address = "10.0.0.1";
+#					prefixLength = 24;
+#				}
+#			];
+#		};
 	};
 
 	# Install setup software
@@ -112,4 +61,70 @@ pactl load-module module-loopback source=MCVirtualSink.monitor sink=alsa_output.
 
 	# Mouse service
 	services.ratbagd.enable = true;
+
+
+
+#	# Network routing
+#	# src: <https://www.reddit.com/r/NixOS/comments/1i89lh2/comment/m8s1g8t/?context=3>
+
+	# Enable kernel packet forwarding
+	boot.kernel.sysctl = {
+		"net.ipv4.conf.all.forwarding" = true;
+#		"net.ipv6.conf.all.forwarding" = true;
+	};
+#
+#	services.dnsmasq = {
+#		enable = true;
+#		resolveLocalQueries = false;
+#		settings = {
+#			# dns
+#			server = [
+#				"193.110.81.0" # https://www.dns0.eu/it
+#				"185.253.5.0"  # https://www.dns0.eu/it
+#			];
+#
+#			domain-needed = true;
+#			bogus-priv = true;
+#			no-resolv = true;
+#			cache-size = 1000;
+#
+#			# dhcp
+#			dhcp-range = [ "br-lan,10.0.0.2,10.0.0.14,1h" ];
+#			dhcp-leasefile = "/var/lib/dnsmasq/dnsmasq.leases";
+#			dhcp-host = "10.0.0.1";
+#
+#			interface = "eno1";
+#			no-hosts = true;
+#		};
+#	};
+#
+#	networking = {
+#		nftables.enable = true;
+#		firewall.trustedInterfaces = [ "eno1" ];
+#		networkmanager.unmanaged = [ "interface-name:eno1" ];
+#
+#		nat = {
+#			enable = true;
+#			internalIPs = [ "10.0.0.0/28" ];
+#			internalInterfaces = [ "eno1" ];
+#
+#			forwardPorts = [ { # 10.0.0.11:22 >>#<< 192.168.7.142:4022
+#				proto = "tcp";
+#				sourcePort = 4022;
+#				destination = "10.0.0.11:22";
+#			} ];
+#
+#			externalInterface = "wlp3s0";
+#		};
+#
+#		interfaces.eno1 = {
+#			useDHCP = false;
+#			ipv4.addresses = [
+#				{
+#					address = "10.0.0.1";
+#					prefixLength = 24;
+#				}
+#			];
+#		};
+#	};
 }
