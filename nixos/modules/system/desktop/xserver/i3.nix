67571d77928360ff7c5337d395b93f8046758bbc
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-02-15 12:36:03 +0100

    NixOS build msi#209

--- nixos/modules/system/desktop/xserver/i3.nix
+++ nixos/modules/system/desktop/xserver/i3.nix
@@ -6,21 +6,22 @@ let cfg = config.system.desktop.xfce; in
 	config = lib.mkIf cfg.enable {
 		services = {
 			displayManager.defaultSession = lib.mkForce "none+i3";
+
 			xserver = {
 
-#MSI:
-# - DVI-D-0 : disconnected
-# - HDMI-0  : connected (DigiQuest)
-# - DP-0    : disconnected
-# - DP-1    : connected (ASUS)
+				#MSI:
+				# - DVI-D-0 : disconnected
+				# - HDMI-0  : connected (DigiQuest)
+				# - DP-0    : disconnected
+				# - DP-1    : connected (ASUS)
 
 				displayManager.setupCommands = let
 					xrandr = "${pkgs.xorg.xrandr}/bin/xrandr";
 				in ''
-${xrandr} --output VGA-1  --mode 1920x1080 --pos 0x0    --rotate normal # Acer
-${xrandr} --output HDMI-0 --mode 1920x1080 --pos 0x0    --rotate normal # DigiQuest
-${xrandr} --output DP-1   --mode 1920x1080 --pos 1920x0 --rotate normal # ASUS
-'';
+					${xrandr} --output VGA-1  --mode 1920x1080 --pos 0x0    --rotate normal # Acer
+					${xrandr} --output HDMI-0 --mode 1920x1080 --pos 0x0    --rotate normal # DigiQuest
+					${xrandr} --output DP-1   --mode 1920x1080 --pos 1920x0 --rotate normal # ASUS
+				'';
 
 				windowManager.i3 = {
 					enable = true;
