61ccb90e2e562a497ef895914f08543e26146b7e
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-03-29 14:08:43 +0100

    NixOS build msi#263: Added msi to msi builders list

--- nixos/hosts/quiss/options.nix
+++ nixos/hosts/quiss/options.nix
@@ -64,6 +64,12 @@
 				};
 				client.builders = [
 					{
+						hostName = "quiss.host.local";
+						maxJobs = 4;
+						features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+						systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
+					}
+					{
 						hostName = "msi.host.local";
 						maxJobs = 6;
 						features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
