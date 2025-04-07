4b4945a6f9be2c631155a1e2045cfbd42eeaec2d
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-04-07 22:04:21 +0200

    NixOS build msi#276: [msi] Added alpha builder

--- nixos/hosts/msi/options.nix
+++ nixos/hosts/msi/options.nix
@@ -62,7 +62,14 @@
 					features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
 					systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
 				};
-				client.builders = [ ];
+				client.builders = [
+					{
+						hostName = "10.0.0.5";
+						maxJobs = 8;
+						features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+						systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
+					}
+				];
 			};
 		};
 	};
