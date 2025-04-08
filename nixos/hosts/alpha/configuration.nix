a77a3481cb3651af9eeff9ebbe4250020b29c5bf
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-04-08 18:26:50 +0200

    Added alpha and beta addresses for PtP communication

--- nixos/hosts/alpha/configuration.nix
+++ nixos/hosts/alpha/configuration.nix
@@ -1,5 +1,21 @@
 { ... }:
 
+let
+	# PtP 10.255.255.252/30
+	alpha-ip = "10.255.255.253";
+	alpha-port-to-beta = "enp3s4f1";
+in
+
 {
+	networking = {
+		interfaces = {
+			"${alpha-port-to-beta}".addresses = [
+				{
+					address = alpha-ip;
+					prefixLength = 30;
+				}
+			];
+		};
+	};
 
 }
--- nixos/hosts/beta/configuration.nix
+++ nixos/hosts/beta/configuration.nix
@@ -1,5 +1,21 @@
 { ... }:
 
+let
+	# PtP 10.255.255.252/30
+	beta-ip  = "10.255.255.254";
+	beta-port-to-alpha = "eno2";
+in
+
 {
+	networking = {
+		interfaces = {
+			"${beta-port-to-alpha}".addresses = [
+				{
+					address = beta-ip;
+					prefixLength = 30;
+				}
+			];
+		};
+	};
 
 }
