2f0ee04ddacf49a99910bf3d56589282f053076b
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-04-16 10:48:27 +0200

    NixOS build jarvis#5: [jarvis] Disabled grub

--- nixos/hosts/jarvis/boot.nix
+++ nixos/hosts/jarvis/boot.nix
@@ -2,8 +2,10 @@
 
 {
 	#Bootloader
-	boot.loader.grub.enable = true;
+	boot.loader.grub.enable = false;
 	boot.loader.grub.device = "/dev/sda";
 	#boot.loader.grub.configurationLimit = 5;
 	boot.loader.grub.useOSProber = false;
+
+	boot.loader.generic-extlinux-compatible.enable = true;
 }
