fe12b729172e11397fa8d8c75e0eb0805e3b0d0b
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-04-18 08:58:13 +0200

    NixOS build jarvis#15: [jarvis] Enabled wpa_supplicant for wlan0

--- nixos/hosts/jarvis/configuration.nix
+++ nixos/hosts/jarvis/configuration.nix
@@ -24,46 +24,41 @@
 			"10.0.0.2"   = [  "switch.local" ];
 
 			# SERVERS
-			"10.0.0.3" = [     "alpha.host.local" ];
-			"10.0.0.4" = [ "alpha-ilo.host.local" ];
+			"10.0.0.3" = [     "alpha.server.local" ];
+			"10.0.0.4" = [ "alpha-ilo.server.local" ];
 
-			"10.0.0.5" = [     "beta.host.local" ];
-			"10.0.0.6" = [ "beta-ilo.host.local" ];
+			"10.0.0.5" = [     "beta.server.local" ];
+			"10.0.0.6" = [ "beta-ilo..local" ];
 
 			"10.0.0.7" = [  "quiss.server.local" ];
 			# "10.0.0.8" = [ "jarvis.server.local" ];
 		};
 
-		networkmanager.enable = lib.mkForce false;
-
-#		wireless = {
-#			enable = lib.mkForce true;
-#			networks."WindTower-LTE".psk = "ext:windtower_lte_psk";
-#			secretsFile = settings.dotfiles_path + "/nixos/secrets/wireless.conf";
-#		};
+		wireless = {
+			enable = lib.mkForce true;
+			networks."WindTower-LTE".psk = "ext:windtower_lte_psk";
+			secretsFile = settings.dotfiles_path + "/nixos/secrets/wireless.conf";
+		};
 
 		nftables.enable = false;
-#		networkmanager.unmanaged = [ "interface-name:wlan0" ]; # "interface-name:enu1u1"
+		networkmanager.unmanaged = [ "interface-name:wlan0" ];
 #		firewall.trustedInterfaces = [ "enu1u1" ];
 
 		interfaces = {
+			wlan0.useDHCP = true;
 #			enu1u1.useDHCP = false;
-			wlan0.useDHCP = false;
 
-			br0.useDHCP = true;      # wlan0 -> gateway
-#			br1 = {
+			# br0 -> enu1u1 -> display
+#			br0 = {
 #				useDHCP = false;
-#				ipv4.addresses = [{  # enu1u1 -> display
+#				ipv4.addresses = [{
 #					address = "192.168.1.25";
 #					prefixLength = 24;
 #				}];
 #			};
 		};
 
-		bridges = {
-			br0.interfaces = [ "wlan0" ];
-#			br1.interfaces = [ "enu1u1" ];
-		};
+#		bridges.br0.interfaces = [ "enu1u1" ];
 	};
 }
 
