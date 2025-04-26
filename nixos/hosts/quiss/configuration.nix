50712f433f9359e88cf9bcb057d98b6e568b9b0e
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-04-26 09:35:39 +0200

    NixOS build quiss#138: [quiss] Enabled calibre

--- nixos/hosts/quiss/configuration.nix
+++ nixos/hosts/quiss/configuration.nix
@@ -198,12 +198,23 @@ PROGRAM "curl -s -X POST -H 'content-type: application/json' -d \"{ \\\"content\
 
 	# BOOK VIEWER
 
-	services.calibre-web = {
-		inherit openFirewall;
-		enable = true;
+	services = 
+	{
+		calibre-server = {
+			inherit openFirewall;
+			enable = true;
 
-		# dataDir = config-dir + "/calibre";
-		group = serv-group;
+			port = 8190;  # 8080
+			group = serv-group;
+		};
+		calibre-web = {
+			inherit openFirewall;
+			enable = true;
+
+			# dataDir = config-dir + "/calibre";
+			listen.port = 8193; # 8083
+			group = serv-group;
+		};
 	};
 
 	# MEDIA PLAYER
