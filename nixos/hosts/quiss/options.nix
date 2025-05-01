e171c6aa86469a243ab8c27585eff3251a69f9d4
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-05-01 09:01:58 +0200

    NixOS build quiss#164: [quiss] disabled dev/net options

--- nixos/hosts/quiss/options.nix
+++ nixos/hosts/quiss/options.nix
@@ -5,36 +5,32 @@
 
 	common.core.network.wakeOnLan.enableFor = [ "eno1" ];
 
-	system = {
-		dev.net.enable = true;
-
-		services = {
-			samba.enable = true;
-			webserver.enable = false;
-			nixbuilder = {
-				server = {
-					enable = true;
-					maxJobs = 4;
-					features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-					systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
-				};
-				client.builders =
-				let
-					gen-builder = (
-						hostName: maxJobs:
-						{
-							inherit hostName maxJobs;
-							features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-							systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
-						}
-					);
-				in
-				[
-					(gen-builder     "msi.host.local" 6)
-					(gen-builder "alpha.server.local" 8)
-					(gen-builder  "beta.server.local" 6)
-				];
+	system.services = {
+		samba.enable = true;
+		webserver.enable = false;
+		nixbuilder = {
+			server = {
+				enable = true;
+				maxJobs = 4;
+				features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+				systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
 			};
+			client.builders =
+			let
+				gen-builder = (
+					hostName: maxJobs:
+					{
+						inherit hostName maxJobs;
+						features = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+						systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "armv7l-linux" "armv6l-linux" ];
+					}
+				);
+			in
+			[
+				(gen-builder     "msi.host.local" 6)
+				(gen-builder "alpha.server.local" 8)
+				(gen-builder  "beta.server.local" 6)
+			];
 		};
 	};
 }
