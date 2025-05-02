4dc739f3777cebf4104e211947b811d1cdb41ede
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-05-02 16:44:27 +0200

    NixOS build quiss#177: Fixed ssh banner indentation typo

--- nixos/common/core/ssh.nix
+++ nixos/common/core/ssh.nix
@@ -136,7 +136,7 @@ Host ${host}
 			openFirewall = true;
 
 			banner = ''
-	You are accessing ${hostname}, one of my computers. DO NOT TOUCH
+You are accessing ${hostname}, one of my computers. DO NOT TOUCH
 '';
 
 			settings = {
