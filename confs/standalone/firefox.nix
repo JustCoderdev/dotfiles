fc498679e735babe7fb77802809fd386774040a3
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-07-25 17:15:21 +0200

    NixOS build msi#363: Removed DuckDuckGo* from firefox

--- confs/standalone/firefox.nix
+++ confs/standalone/firefox.nix
@@ -152,15 +152,15 @@ in
 
 			SearchBar = "unified";
 			SearchEngines = {
-				Default = "DuckDuckGo*";
-				Remove = [ "DuckDuckGo" "Bing" "Ecosia" "Qwant" "Wikipedia" ];
-				Add = [ {
-					Name = "DuckDuckGo*";
-					URLTemplate = "https://duckduckgo.com/{searchTerms}&kah=uk-en&k1=-1&k5=2&kak=-1&kao=-1&kap=-1&kaq=-1&kau=-1&kax=-1&kbe=0&kbg=-1&kl=wt-wt&kp=-2&kpsb=-1&kz=-1&kaj=m";
-					Method = "GET";
-					IconURL = "https://duckduckgo.com/favicon.ico";
-					Alias = "@ddg";
-				} ];
+				Default = "DuckDuckGo";
+				Remove = [ "Bing" "Ecosia" "Qwant" "Wikipedia" ];
+				# Add = [ {
+				# 	Name = "DuckDuckGo*";
+				# 	URLTemplate = "https://duckduckgo.com/{searchTerms}&kah=uk-en&k1=-1&k5=2&kak=-1&kao=-1&kap=-1&kaq=-1&kau=-1&kax=-1&kbe=0&kbg=-1&kl=wt-wt&kp=-2&kpsb=-1&kz=-1&kaj=m";
+				# 	Method = "GET";
+				# 	IconURL = "https://duckduckgo.com/favicon.ico";
+				# 	Alias = "@ddg";
+				# } ];
 			};
 			SearchSuggestEnabled = true;
 
