c7d1ccff54aef023f2d624dc232322c6294d0736
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-08-20 22:08:45 +0200

    NixOS build jarvis#38: [jarvis] Migrated from 128GB SD card to 32GB one

--- nixos/hosts/jarvis/hardware-configuration.nix
+++ nixos/hosts/jarvis/hardware-configuration.nix
@@ -7,19 +7,19 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
- 
-  boot.initrd.availableKernelModules = [ ];
+
+  boot.initrd.availableKernelModules = [ "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
- 
+
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
     };
- 
+
   swapDevices = [ ];
- 
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -27,6 +27,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enu1u1.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
- 
+
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
