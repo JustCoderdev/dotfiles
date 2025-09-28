65ac9d2b6952ffe02e6bd10438b438d03ad247ae
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-09-28 11:55:58 +0200

    NixOS build msi#436: [msi] Added iPhone TP 3.0 public key to wireguard

--- nixos/hosts/msi/configuration.nix
+++ nixos/hosts/msi/configuration.nix
@@ -49,7 +49,8 @@
 				);
 			in
 			{
-				quiss = (add-peer "UQYuZhhWWm2kYNXeoIxb+50Dv/XYb9bQDFc8DTSFbT0=" "10.255.250.2");
+				        quiss = (add-peer "UQYuZhhWWm2kYNXeoIxb+50Dv/XYb9bQDFc8DTSFbT0=" "10.255.250.2");
+				iphone-tp-2_0 = (add-peer "WUEqbbv7RGfw9EhKjPDeZqwkuKwsODsdTtvMv7Gt+Vk=" "10.255.250.3");
 			};
 		};
 	};
