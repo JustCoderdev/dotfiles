2413442590932fde6ed1343873a0602939eecbb2
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-10-11 19:13:32 +0200

    NixOS build msi#473: Updated i3 colors to be more bright?

--- confs/stylix/base.nix
+++ confs/stylix/base.nix
@@ -26,10 +26,10 @@ in
 					base05 = "#d5c4a1"; # ++
 					base06 = "#ebdbb2"; # +++
 					base07 = "#fbf1c7"; # ++++
-					base08 = "#fb4934"; # red
+					base08 = "#e94134"; # red     #fb4934
 					base09 = "#fe8019"; # orange
 					base0A = "#fabd2f"; # yellow
-					base0B = "#b8bb26"; # green
+					base0B = "#dce052"; # green   #b8bb26
 					base0C = "#8ec07c"; # aqua/cyan
 					base0D = "#83a598"; # blue
 					base0E = "#d3869b"; # purple
