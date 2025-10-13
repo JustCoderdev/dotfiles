ae54f3cbd258a7ab5281430884e6f6a52502d0b9
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-10-13 12:55:19 +0200

    NixOS build msi#479: [msi] Removed swapfile

--- nixos/hosts/msi/hardware-configuration.nix
+++ nixos/hosts/msi/hardware-configuration.nix
@@ -24,9 +24,7 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/f96ee595-055a-455a-9ef3-55766c7ca110"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
