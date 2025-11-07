3b9ce17086ce28acb376e7f606113a65e94cb4ec
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-11-07 20:49:18 +0100

    NixOS build msi#24: Added wheel group to nix trusted-users

--- nixos/common/core/nix.nix
+++ nixos/common/core/nix.nix
@@ -39,6 +39,7 @@ in
 				narinfo-cache-negative-ttl = 2;                      # If a store path is queried from a substituter but was not found, there will be a negative lookup cached in the local disk cache database for the specified duration
 				narinfo-cache-positive-ttl = 0;                      # If a store path is queried from a substituter, the result of the query will be cached in the local disk cache database including some of the NAR metadata
 
+				trusted-users = [ "@wheel" ];
 				trusted-public-keys =
 				[
 					# TODO: Update keys
