46facfbabf47803529a5f9cb6e77f0254d451600
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-12-23 19:26:47 +0100

    NixOS build asus#130: Removed gnome

--- nixos/hosts/asus/configuration.nix
+++ nixos/hosts/asus/configuration.nix
@@ -1,15 +1,20 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-	environment.systemPackages = with pkgs; [ dbeaver-bin vscode ];
+	environment.systemPackages = with pkgs; [ dbeaver-bin vscode uxplay ];
+	networking.firewall.allowedTCPPorts = [ 7000 7100 ];
+	networking.firewall.allowedUDPPorts = [ 6000 6001 7011 ];
 
-	services.xserver.displayManager.lightdm.greeters.gtk.indicators = lib.mkForce null;
-	services.xserver.desktopManager.gnome.enable = true;
-
-	services.gnome.games.enable = false;
-
-	common.core.audio.backend = "pipewire";
-	services.tlp.enable = false;
+	# Gnome
+	# -------------------- #
+	# services.xserver.displayManager.lightdm.greeters.gtk.indicators = lib.mkForce null;
+	# services.xserver.desktopManager.gnome.enable = true;
+	#
+	# services.gnome.games.enable = false;
+	#
+	# common.core.audio.backend = "pipewire";
+	# services.tlp.enable = false;
+	# -------------------- #
 
 	services.mysql = {
 		enable = true;
