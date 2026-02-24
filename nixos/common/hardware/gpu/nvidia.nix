7c72b680d8367d2917d6cab13b794c07c6b837e8
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2026-02-24 17:29:34 +0100

    NixOS build msi#91: Re-enabled nvidia power management

--- nixos/common/hardware/gpu/nvidia.nix
+++ nixos/common/hardware/gpu/nvidia.nix
@@ -64,7 +64,7 @@ in
 			};
 
 			powerManagement = {
-				enable = false; # EXPERIMENTAL: enable if issues with sleep/suspend
+				enable = true; # EXPERIMENTAL: enable if issues with sleep/suspend
 				finegrained = false && self-ngpu.ge-turing;  # gpu off when idle (Turing or newer)
 			};
 		};
