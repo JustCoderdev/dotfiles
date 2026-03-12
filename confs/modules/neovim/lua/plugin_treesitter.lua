296b980eca98454355e44e575930561bad754fd8
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2026-03-12 16:22:05 +0100

    NixOS build asus#152: jcconfs:neovim: Fixed treesitter versions incompatibility

--- confs/modules/neovim/lua/plugin_treesitter.lua
+++ confs/modules/neovim/lua/plugin_treesitter.lua
@@ -1,24 +1,58 @@
 declare_file("plugin:treesitter")
 
-require('nvim-treesitter.install').compilers = { "gcc" }
-require("nvim-treesitter.config").setup(
+local ensure_installed_langs =
 {
-	ensure_installed =
+	-- languages
+	"c", "make",
+	"html", "css", "javascript", "typescript",
+
+	-- data format
+	"toml", "json", "jsonc", "yaml", "ini",
+
+	-- other
+	"bash", "lua", "nix",
+	"comment", "regex",
+	"dockerfile", "passwd",
+	"git_config", "git_rebase", "gitignore", "diff",
+	"markdown", "markdown_inline", "sql"
+};
+
+local using_configs, configs = pcall(require, "nvim-treesitter.configs")
+local using_config, config = pcall(require, "nvim-treesitter.config")
+
+if using_config and using_configs then
+	print("treesitter: using both config and configs?, quitting...")
+	return
+end
+
+-- New configuration
+-- ------------------------------------------------------------
+
+if not using_configs and not using_config
+then
+	print("treesitter: new configuration")
+	local treesitter = require("nvim-treesitter")
+	treesitter.setup()
+	treesitter.install(ensure_installed_langs)
+	vim.api.nvim_create_autocmd('FileType',
 	{
-		-- languages
-		"c", "make",
-		"html", "css", "javascript", "typescript",
+		pattern = { ".*" },
+		callback = function() vim.treesitter.start() end,
+	})
+	return
+end
 
-		-- data format
-		"toml", "json", "jsonc", "yaml", "ini",
+if not using_configs and using_config then
+	print("treesitter: using config")
+	configs = config
+else
+	print("treesitter: using configs")
+end
 
-		-- other
-		"bash", "lua", "nix",
-		"comment", "regex",
-		"dockerfile", "passwd",
-		"git_config", "git_rebase", "gitignore", "diff",
-		"markdown", "markdown_inline", "sql"
-	},
+require('nvim-treesitter.install').compilers = { "gcc" }
+configs.setup(
+{
+	ensure_installed = ensure_installed_langs,
 
 	sync_install = true,
 	auto_install = true,
