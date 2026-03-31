b20bbdae8381870734d02ec545a2cf4aafd256c8
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2026-03-31 14:59:34 +0200

    NixOS build msi#107: Removed unused noto emoji font

--- nixos/common/core/fonts.nix
+++ nixos/common/core/fonts.nix
@@ -17,13 +17,11 @@ in
 			enableDefaultPackages = false;
 			packages = with pkgs;
 			[
-				ttf_bitstream_vera  # dejavu_fonts
-				roboto-mono
-				twitter-color-emoji # Emoji support
+				ttf_bitstream_vera
+				roboto-mono         # Monospace
 
-				# Noto fonts
-				noto-fonts-cjk-sans
-				noto-fonts-color-emoji
+				noto-fonts-cjk-sans # Chinese, Japanese, and Korean support
+				twitter-color-emoji # Emoji support
 			];
 
 			###########################################################
@@ -32,9 +30,9 @@ in
 
 			fontconfig.defaultFonts =
 			{
-				sansSerif = [ "Bitstream Vera Sans"  "Noto Sans CJK JP"  ]; # "DejaVu Sans"
-				serif     = [ "Bitstream Vera Serif" "Noto Serif CJK JP" ]; # "DejaVu Serif"
-				monospace = [ "Roboto Mono"  "Noto Mono CJK JP"  ];
+				sansSerif = [ "Bitstream Vera Sans"  "Noto Sans CJK JP"  ];
+				serif     = [ "Bitstream Vera Serif" "Noto Serif CJK JP" ];
+				monospace = [ "Roboto Mono"          "Noto Mono CJK JP"  ];
 				emoji     = [ "Twitter Color Emoji" ];
 				# ⚫ 🔴 🔵
 			};
