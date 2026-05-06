6b19d2b9fd601e587da519e6460371f7eda88ff8
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2026-05-06 11:35:24 +0200

    NixOS build asus#171: Updated again serial configuration

--- nixos/common/core/console.nix
+++ nixos/common/core/console.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+let
+	serial-devices = [ "ttyUSB0" "ttyUSB1" ];
+in
+
 {
 	# Configure console keymap
 	console =
@@ -16,7 +20,9 @@
 	# ------------------------------------------------------------ #
 	# Source: <https://wiki.nixos.org/wiki/Serial_Console>
 
-	boot.kernelParams = [ "console=ttyUSB0,115200n8" ];
+	boot.kernelParams = [ ]
+		++ builtins.map (dev: "console=${dev},115200n8") serial-devices
+		;
 
 	# Enable early console output during boot
 	boot.consoleLogLevel = 7;  # Show all kernel messages
@@ -30,31 +36,43 @@ terminal_output serial
 '';
 
 	# Disable the upstream getty module's automatic configuration for serial-getty@
-	systemd.services."serial-getty@".enable = false;
+	# systemd.services."serial-getty@".enable = false;
 
 	# Configure our own serial-getty@ttyS0 service
-	systemd.services."serial-getty@ttyUSB0" =
-	{
-		enable = true;
-		wantedBy = [ "getty.target" ];
-		after = [ "systemd-user-sessions.service" ];
-		wants = [ "systemd-user-sessions.service" ];
-		serviceConfig =
-		{
-			Type = "idle";
-			Restart = "always";
-			ExecStart = "${pkgs.util-linux}/bin/agetty --login-program ${pkgs.shadow}/bin/login --noclear --keep-baud ttyUSB0 115200,57600,38400,9600 vt220";
-			UtmpIdentifier = "ttyUSB0";
-			StandardInput = "tty";
-			StandardOutput = "tty";
-			TTYPath = "/dev/ttyUSB0";
-			TTYReset = "yes";
-			TTYVHangup = "yes";
-			IgnoreSIGPIPE = "no";
-			SendSIGHUP = "yes";
-		};
-		# environment.TERM = "xterm-256color";
-	};
+	systemd.services = { "serial-getty@".enable = false; }
+		// builtins.listToAttrs
+		(
+			builtins.map
+			(
+				dev:
+				{
+					name = "serial-getty@${dev}";
+					value =
+					{
+						enable = true;
+						wantedBy = [ "getty.target" ];
+						after = [ "systemd-user-sessions.service" ];
+						wants = [ "systemd-user-sessions.service" ];
+						serviceConfig =
+						{
+							Type = "idle";
+							Restart = "always";
+							ExecStart = "${pkgs.util-linux}/bin/agetty --login-program ${pkgs.shadow}/bin/login --noclear --keep-baud ${dev} 115200,57600,38400,9600 vt220";
+							UtmpIdentifier = dev;
+							StandardInput = "tty";
+							StandardOutput = "tty";
+							TTYPath = "/dev/${dev}";
+							TTYReset = "yes";
+							TTYVHangup = "yes";
+							IgnoreSIGPIPE = "no";
+							SendSIGHUP = "yes";
+						};
+					};
+				}
+			)
+			serial-devices
+		)
+		;
 
 	# Serial
 	# Source: <https://github.com/nix-community/srvos/blob/main/nixos/common/serial.nix>
