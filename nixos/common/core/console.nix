be048461ad8d587cd90d7ec9879b283b08d0f456
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2026-05-06 10:47:00 +0200

    NixOS build asus#168: Updated serial to work with USB

--- nixos/common/core/console.nix
+++ nixos/common/core/console.nix
@@ -16,7 +16,7 @@
 	# ------------------------------------------------------------ #
 	# Source: <https://wiki.nixos.org/wiki/Serial_Console>
 
-	boot.kernelParams = [ "console=ttyS0,115200n8" "console=tty1" ];
+	boot.kernelParams = [ "console=ttyUSB0,115200n8" "console=tty1" ];
 
 	# Enable early console output during boot
 	boot.consoleLogLevel = 7;  # Show all kernel messages
@@ -33,7 +33,7 @@ terminal_output serial
 	systemd.services."serial-getty@".enable = false;
 
 	# Configure our own serial-getty@ttyS0 service
-	systemd.services."serial-getty@ttyS0" =
+	systemd.services."serial-getty@ttyUSB0" =
 	{
 		enable = true;
 		wantedBy = [ "getty.target" ];
@@ -43,11 +43,11 @@ terminal_output serial
 		{
 			Type = "idle";
 			Restart = "always";
-			ExecStart = "${pkgs.util-linux}/bin/agetty --login-program ${pkgs.shadow}/bin/login --noclear --keep-baud ttyS0 115200,57600,38400,9600 vt220";
-			UtmpIdentifier = "ttyS0";
+			ExecStart = "${pkgs.util-linux}/bin/agetty --login-program ${pkgs.shadow}/bin/login --noclear --keep-baud ttyUSB0 115200,57600,38400,9600 vt220";
+			UtmpIdentifier = "ttyUSB0";
 			StandardInput = "tty";
 			StandardOutput = "tty";
-			TTYPath = "/dev/ttyS0";
+			TTYPath = "/dev/ttyUSB0";
 			TTYReset = "yes";
 			TTYVHangup = "yes";
 			IgnoreSIGPIPE = "no";
