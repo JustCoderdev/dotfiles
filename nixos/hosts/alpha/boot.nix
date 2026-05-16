99077700bd0bf9e237f809984db01c8250544205
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2026-05-16 20:36:39 +0200

    NixOS build alpha#16: [alpha] Disables serial

--- nixos/hosts/alpha/boot.nix
+++ nixos/hosts/alpha/boot.nix
@@ -4,10 +4,10 @@
 	common.core.bootloader.grub.enable = true;
 	boot.loader.grub.device = "/dev/sda";
 
-	boot.kernelParams = [ "console=ttyS0,115200n8" ];
-	boot.loader.grub.extraConfig = ''
-serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
-terminal_input serial
-terminal_output serial
-'';
+	# boot.kernelParams = [ "console=ttyS0,115200n8" ];
+	# boot.loader.grub.extraConfig = ''
+# serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+# terminal_input serial
+# terminal_output serial
+# '';
 }
